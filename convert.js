2d0818175be6c4a8b6dc7529ad8a5a505a3699e9
Chris Achard <chrisachard@gmail.com>
2017-10-03 12:46:36 -0400

    Remove console statement

--- convert.js
+++ convert.js
@@ -30,8 +30,6 @@ const INPUT_FILENAME = pathArray[pathArray.length - 1]
 const INPUT_FILE_NO_SPACES = INPUT_FILENAME.replace(/\s/g, '_').split(".svg")[0]
 const OUTPUT_FILE = INPUT_FILE_NO_SPACES.split(".svg")[0] + ".js"
 
-console.warn("OUTPUT_FILE", OUTPUT_FILE)
-
 const OUTPUT_DIR = './output'
 const TEMP_DIR = './temp'
 const IMAGES_DIR = INPUT_FILE_NO_SPACES.split(".svg")[0]+'_images'
